af107209c670df3a5a44120af0b03a021f612162
SayeedJ <sayeedjoseph@gmail.com>
2020-06-18 23:04:32 -0400

    chore: change PORT

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { db } = require('./server/db/');
 const app = require('./server/server');
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 
 db.sync({ force: false })
   .then(() => {
